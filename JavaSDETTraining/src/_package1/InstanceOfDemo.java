ba72d4bc93afe91a4f12d3950031b83923706c8b
Anis Subba <21302329@student.uwl.ac.uk>
2019-09-30 15:03:19 +0100

    Instance of demo

--- JavaSDETTraining/src/_package1/InstanceOfDemo.java
+++ JavaSDETTraining/src/_package1/InstanceOfDemo.java
@@ -6,6 +6,14 @@ public class InstanceOfDemo {
 
         Parent obj1 = new Parent();
         Parent obj2 = new Child();
+
+        System.out.println("obj1 is an instance of Parent : " + (obj1 instanceof Parent));
+        System.out.println("obj1 is an instance of Child : " + (obj1 instanceof Child));
+        System.out.println("obj1 is an instance of MyInterface : " + (obj1 instanceof MyInterface));
+
+        System.out.println("obj2 is an instance of Parent : " + (obj2 instanceof Parent));
+        System.out.println("obj2 is an instance of Child : " + (obj2 instanceof Child));
+        System.out.println("obj2 is an instance of MyInterface : " + (obj2 instanceof MyInterface));
     }
 }
 
