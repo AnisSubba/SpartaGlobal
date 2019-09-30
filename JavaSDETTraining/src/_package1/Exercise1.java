7fdee7c6a59a492a52cd61b0e5dc3f71a361871e
Anis Subba <21302329@student.uwl.ac.uk>
2019-09-30 11:39:01 +0100

    Initial commit - Greetings

---
+++ JavaSDETTraining/src/_package1/Exercise1.java
@@ -0,0 +1,25 @@
+package _package1;
+
+import java.util.Arrays;
+
+public class Exercise1 {
+    public static void main(String[] args){
+        char[] myArray;
+        myArray = new char[8];
+
+        myArray[0] = 'p';
+        myArray[1] = 'a';
+        myArray[2] = 's';
+        myArray[3] = 's';
+        myArray[4] = 'w';
+        myArray[5] = 'o';
+        myArray[6] = 'r';
+        myArray[7] = 'd';
+
+        char[] firstArray = Arrays.copyOfRange(myArray, 0, myArray.length);
+
+        for (char i = 4; i < firstArray.length; i++){
+            System.out.println(firstArray[i]);}
+
+    }
+}
