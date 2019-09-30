8641f3e552a369850162e2c566bcf13fe0535601
Anis Subba <21302329@student.uwl.ac.uk>
2019-09-30 09:47:01 +0100

    Initial commit - Greetings

--- JavaSDETTraining/src/_package1/Greetings.java
+++ JavaSDETTraining/src/_package1/Greetings.java
@@ -3,6 +3,7 @@ package _package1;
 public class Greetings {
 
     public static void main(String[] args) {
-        System.out.println("this is a greetings on monday morning!");
+        System.out.println("hello world");
+        System.out.println("hi, how you doing.");
     }
 }
