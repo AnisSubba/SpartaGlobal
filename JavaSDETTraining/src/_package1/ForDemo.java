3d030e2565947403638febf5f280244b814fbd5e
Anis Subba <21302329@student.uwl.ac.uk>
2019-09-30 15:28:37 +0100

    for demo

---
+++ JavaSDETTraining/src/_package1/ForDemo.java
@@ -0,0 +1,9 @@
+package _package1;
+
+public class ForDemo {
+    public static void main(String[] args) {
+        for(int i = 2; i < 11; i++){
+            System.out.println("Count is : " + i);
+        }
+    }
+}
