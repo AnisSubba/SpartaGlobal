3fd0a7909534d84dec752d4318f3020a5c373531
Anis Subba <21302329@student.uwl.ac.uk>
2019-09-30 16:49:49 +0100

    exercises

--- JavaSDETTraining/src/_package1/Exercise2.java
+++ JavaSDETTraining/src/_package1/Exercise2.java
@@ -2,7 +2,7 @@ package _package1;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
