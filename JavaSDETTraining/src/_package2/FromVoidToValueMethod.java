24f9f031627a71deb8b07781738919843e3e938c
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-01 14:46:19 +0100

    Void to value Method Demo

---
+++ JavaSDETTraining/src/_package2/FromVoidToValueMethod.java
@@ -0,0 +1,23 @@
+package _package2;
+
+public class FromVoidToValueMethod {
+    public static char getGrade(double score){
+        // from void to value method
+        if(score >= 90.0)
+            return 'A';
+        else if (score >= 80.0)
+            return 'B';
+        else if (score >= 70.0)
+            return 'C';
+        else if (score >= 60.0)
+            return 'D';
+        else
+            return 'F';
+    }
+    public static void main (String[] args){
+        // since it is value you can insert it to the out.println method
+        System.out.println("the grade is:" + getGrade(60.5));
+        System.out.println("The grade is:" + getGrade(85.2));
+
+    }
+}
