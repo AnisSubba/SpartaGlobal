d2007c6fc8b8a1c20dc09058fe7516be080f8147
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-01 16:19:43 +0100

    GCD with Modular

---
+++ JavaSDETTraining/src/_package2/GCDWithModular.java
@@ -0,0 +1,28 @@
+package _package2;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class GCDWithModular {
+    public static int gcd(int n1, int n2){
+        int gcd = 1;
+        int k = 2;
+
+        while(k <= n1 && k <= n2){
+            if(n1 % k == 0 && n2 % k == 0)
+                gcd = k;
+            k++;
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter First Integer: ");
+        int n1 = input.nextInt();
+        System.out.println("Enter Second Integer: ");
+        int n2 = input.nextInt();
+
+        System.out.println("The Greatest commond divisor for " + n1 + " and " + n2 + " is " + gcd(n1, n2));
+    }
+}
