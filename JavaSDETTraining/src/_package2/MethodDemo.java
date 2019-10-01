a4c38213931fa8a9a832efeee600b76a04a4ec53
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-01 14:18:39 +0100

    Method Demo

---
+++ JavaSDETTraining/src/_package2/MethodDemo.java
@@ -0,0 +1,17 @@
+package _package2;
+
+public class MethodDemo {
+    public static int max(int num1, int num2){
+        int result;
+        if(num1 > num2){
+            result = num1;
+        }else {
+            result =num2;
+
+        }
+        return result;
+    }
+    public static void main(String[] args){
+
+    }
+}
