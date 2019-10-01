008da9ce5807a64d4c59c3d6bf304d9508364fd4
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-01 12:07:33 +0100

    Do While Demo

---
+++ JavaSDETTraining/src/_package1/DoWhileDemo.java
@@ -0,0 +1,12 @@
+package _package1;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int count = 1;
+
+        do {
+            System.out.println("count is : " + count);
+            count++;
+        } while (count < 11);
+    }
+}
