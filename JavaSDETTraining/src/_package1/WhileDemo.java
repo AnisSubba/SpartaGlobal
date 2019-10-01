9d2521dfc7ff25b1c665da10f45e7a561459fc7d
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-01 11:29:34 +0100

    while loop

---
+++ JavaSDETTraining/src/_package1/WhileDemo.java
@@ -0,0 +1,12 @@
+package _package1;
+
+public class WhileDemo {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+            // will increment the count(1) by adding 1.
+        }
+    }
+}
