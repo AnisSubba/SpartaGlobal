6895fa60e9ead337a47e284a41b481cd1cde4d9f
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-01 14:23:01 +0100

    Method Demo

--- JavaSDETTraining/src/_package2/MethodDemo.java
+++ JavaSDETTraining/src/_package2/MethodDemo.java
@@ -1,6 +1,7 @@
 package _package2;
 
 public class MethodDemo {
+    // stating the value returning methods
     public static int max(int num1, int num2){
         int result;
         if(num1 > num2){
@@ -12,6 +13,9 @@ public class MethodDemo {
         return result;
     }
     public static void main(String[] args){
-
+        int z = max(4,7);
+        System.out.println(z);
+        // or
+        System.out.println(max(6, 5));
     }
 }
