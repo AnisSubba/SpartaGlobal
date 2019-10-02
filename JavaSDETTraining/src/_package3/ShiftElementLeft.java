3d8696fa2b6440381cee92d9a935b3db3835f458
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-02 11:58:17 +0100

    Shifting element to the left one place!

---
+++ JavaSDETTraining/src/_package3/ShiftElementLeft.java
@@ -0,0 +1,22 @@
+package _package3;
+
+import java.util.Arrays;
+
+public class ShiftElementLeft {
+    public static void main(String[] args) {
+        int[] myNumbers = {21,25,64,15,75,12,56,34,2,5,8,41,2,5,45};
+
+        System.out.println(Arrays.toString(myNumbers));
+
+//        to shift the element to the left by 1 place
+        int temp = myNumbers[0];
+        for (int i = 1; i < myNumbers.length; i++){
+            myNumbers[i-1] = myNumbers[i];
+        }
+        // to shift the first element to be the last element:
+        myNumbers[myNumbers.length -1] = temp;
+
+        System.out.println(Arrays.toString(myNumbers));
+    }
+
+}
